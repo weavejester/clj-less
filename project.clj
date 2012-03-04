792288943dc392b3d5ae21dd1064037d47c3d619
James Reeves <jreeves@weavejester.com>
2012-03-04 23:01:32 +0000

    Updated evaljs dependency to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-less "0.1.0-SNAPSHOT"
   :description "LESS interpreter for Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [evaljs "0.1.0"]])
+                 [evaljs "0.1.2"]])
